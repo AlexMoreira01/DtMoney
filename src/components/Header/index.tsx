0d9df8e3e5c0d34135a72512da380e894cf59939
Alex Moreira <alex.moreira.andrade@hotmail.com>
2023-03-13 23:32:25 -0300

    Feature: Modal with radix ui

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,36 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import * as Dialog from '@radix-ui/react-dialog';
 
 import logoImg from '../../assets/logo.svg';
 
-export function Header(){
+export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
 
         <img src={logoImg} alt="" />
 
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <Dialog.Root>
+          {/* aschild ira mudar para que o dialog trigger não crie um novo botão e utilize 
+          o botao dentro da tag que ele esta */}
+          <Dialog.Trigger asChild>
+
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay>
+              <Dialog.Content>
+                <Dialog.Title>Nova transação</Dialog.Title>
+
+                <Dialog.Close />
+              </Dialog.Content>
+            </Dialog.Overlay>
+          </Dialog.Portal>
+        </Dialog.Root>
+
       </HeaderContent>
-      
+
     </HeaderContainer>
   )
 }
