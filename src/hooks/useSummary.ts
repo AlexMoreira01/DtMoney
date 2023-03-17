0619c86717b5e3929295501ef8ba290f6d0df6cc
Alex Moreira <alex.moreira.andrade@hotmail.com>
2023-03-16 23:37:55 -0300

    Feature: formatters for date and values, init hook form

---
+++ src/hooks/useSummary.ts
@@ -0,0 +1,31 @@
+import { useContext } from "react";
+import { TransactionsContext } from "../contexts/TransactionsContext";
+
+export function useSummary() {
+  const { transactions } = useContext(TransactionsContext)
+
+  // Ira se reduzir um array para uma outra estrutura
+  // Primiero se passa uma função e o segundo e a estrutura que queremos formar
+  // acc => ao objeto formado
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+
+      if (transaction.type === 'income') {
+        acc.income += transaction.price;
+        acc.total += transaction.price;
+      } else {
+        acc.outcome += transaction.price;
+        acc.total -= transaction.price;
+      }
+
+      return acc
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0
+    }
+  )
+
+  return summary;
+}
