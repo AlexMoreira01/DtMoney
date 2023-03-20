86497bd52afd9d55f9062ee62f093df19c9d96ca
Alex Moreira <alex.moreira.andrade@hotmail.com>
2023-03-19 23:31:51 -0300

    Feature: component summary and methods for format

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,5 +1,5 @@
-import { useContext } from "react";
-import { TransactionsContext } from "../contexts/TransactionsContext";
+import { useContext } from 'react'
+import { TransactionsContext } from '../contexts/TransactionsContext'
 
 export function useSummary() {
   const { transactions } = useContext(TransactionsContext)
@@ -9,13 +9,12 @@ export function useSummary() {
   // acc => ao objeto formado
   const summary = transactions.reduce(
     (acc, transaction) => {
-
       if (transaction.type === 'income') {
-        acc.income += transaction.price;
-        acc.total += transaction.price;
+        acc.income += transaction.price
+        acc.total += transaction.price
       } else {
-        acc.outcome += transaction.price;
-        acc.total -= transaction.price;
+        acc.outcome += transaction.price
+        acc.total -= transaction.price
       }
 
       return acc
@@ -23,9 +22,9 @@ export function useSummary() {
     {
       income: 0,
       outcome: 0,
-      total: 0
-    }
+      total: 0,
+    },
   )
 
-  return summary;
+  return summary
 }
