1b6350294ee45a6e8a53ce7e60ded43dde344195
Alex Moreira <alex.moreira.andrade@hotmail.com>
2023-03-23 21:10:31 -0300

    Feature: Memo in searchForm - useSummary

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,6 +1,6 @@
 import { useContextSelector } from "use-context-selector";
-import { useContext } from "react";
 import { TransactionsContext } from "../contexts/TransactionsContext";
+import { useMemo } from "react";
 
 export function useSummary() {
   const transactions = useContextSelector(TransactionsContext, (context) => {
@@ -10,24 +10,26 @@ export function useSummary() {
   // Ira se reduzir um array para uma outra estrutura
   // Primiero se passa uma função e o segundo e a estrutura que queremos formar
   // acc => ao objeto formado
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === "income") {
-        acc.income += transaction.price;
-        acc.total += transaction.price;
-      } else {
-        acc.outcome += transaction.price;
-        acc.total -= transaction.price;
-      }
+  const summary = useMemo(() => {
+    return transactions.reduce(
+      (acc, transaction) => {
+        if (transaction.type === "income") {
+          acc.income += transaction.price;
+          acc.total += transaction.price;
+        } else {
+          acc.outcome += transaction.price;
+          acc.total -= transaction.price;
+        }
 
-      return acc;
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    }
-  );
+        return acc;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      }
+    );
+  }, [transactions]);
 
   return summary;
 }
